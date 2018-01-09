a434a72058439caf40cb193a375f333ebbb971b1
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-01-08 17:47:33 -0800

    create user options at start

--- ratings.py
+++ ratings.py
@@ -11,15 +11,34 @@ with open('scores.txt') as restaurant_data:
 # lambda statement = sort by rating
 # sorted_rs = sorted(restaurants.items(), key=lambda tup: tup[1])
 
-
 def print_sorted_restaurants(restaurant_data):
     for restaurant, rating in sorted(restaurant_data.items()):
-        print restaurant, "is rated at", rating + "."
+        print restaurant, "is rated at", str(rating) + "."
 
-restaurant = raw_input(("Which respectable nutrient insertion factory would"
-                        " you like to judge? ")).title()
-rating = raw_input("What is your standard good sir/madame? ")
 
-restaurants[restaurant] = rating
+def add_restaurant():
+    restaurant = raw_input(("Which respectable nutrient insertion factory would"
+                            " you like to judge? ")).title()
+    while True:
+        rating = int(raw_input("What is your standard good sir/madame? "))
+        if 1 > rating or rating > 5:
+            print "Oops, rating out of range!"
+            continue
+        else:
+            break
 
-print_sorted_restaurants(restaurants)
+    restaurants[restaurant] = rating
+
+while True:
+    desire = raw_input("What would you like to do?\n"
+                       "a: See all ratings (alphabetical by restaurant)\n"
+                       "b: Add new restaurant\n"
+                       "c: Quit\n"
+                       ">")
+
+    if desire.lower() == 'a':
+        print_sorted_restaurants(restaurants)
+    elif desire.lower() == 'b':
+        add_restaurant()
+    elif desire.lower() == 'c':
+        quit()
