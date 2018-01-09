eadb062412b8fc00370d8af980b564a2fba7eeb9
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-01-08 16:29:10 -0800

    adds user prompt and adds new restaurant data to dictionary

--- ratings.py
+++ ratings.py
@@ -3,7 +3,7 @@
 # restaurants_data = open('scores.txt')
 restaurants = {}
 
-with open('scores.txt') as restaurant_data:    
+with open('scores.txt') as restaurant_data:
     for line in restaurant_data:
         restaurant, rating = line.rstrip().split(":")
         restaurants[restaurant] = rating
@@ -11,13 +11,15 @@ with open('scores.txt') as restaurant_data:
 # lambda statement = sort by rating
 # sorted_rs = sorted(restaurants.items(), key=lambda tup: tup[1])
 
+
 def print_sorted_restaurants(restaurant_data):
     for restaurant, rating in sorted(restaurant_data.items()):
         print restaurant, "is rated at", rating + "."
 
-restaurant = raw_input("which respectable nutrient insertion factory would\
-                             you like to judge? ")
-rating = raw_input("what is your standard good sir/madame? ")
+restaurant = raw_input(("Which respectable nutrient insertion factory would"
+                        " you like to judge? ")).title()
+rating = raw_input("What is your standard good sir/madame? ")
 
 restaurants[restaurant] = rating
+
 print_sorted_restaurants(restaurants)
