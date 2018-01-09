d8b1d91875d70e36e4cf8d2e006a6f98d884a7c7
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-01-08 16:24:05 -0800

    sort and filter restaurants by name and give rating

--- ratings.py
+++ ratings.py
@@ -1,12 +1,23 @@
 """Restaurant rating lister."""
 
-restaurants_f = open('scores.txt')
+# restaurants_data = open('scores.txt')
 restaurants = {}
-for line in restaurants_f:
-    restaurant_name, rating = line.rstrip().split(":")
-    restaurants[restaurant_name] = rating
 
-sorted_rs = sorted(restaurants.items())
+with open('scores.txt') as restaurant_data:    
+    for line in restaurant_data:
+        restaurant, rating = line.rstrip().split(":")
+        restaurants[restaurant] = rating
 
-for restaurant, rating in sorted_rs:
-    print restaurant, "is rated at", rating + "."
+# lambda statement = sort by rating
+# sorted_rs = sorted(restaurants.items(), key=lambda tup: tup[1])
+
+def print_sorted_restaurants(restaurant_data):
+    for restaurant, rating in sorted(restaurant_data.items()):
+        print restaurant, "is rated at", rating + "."
+
+restaurant = raw_input("which respectable nutrient insertion factory would\
+                             you like to judge? ")
+rating = raw_input("what is your standard good sir/madame? ")
+
+restaurants[restaurant] = rating
+print_sorted_restaurants(restaurants)
